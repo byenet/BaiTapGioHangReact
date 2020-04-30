7d255cc79d6758970acec4be846175d101855f9f
byenet <nhathuynguyenho@gmail.com>
2020-04-30 10:25:20 +0700

    Updates

---
+++ precache-manifest.4e6be58a304448a39797ba58d73e7df7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db91a6f2b826ab6282905109431c5d56",
+    "url": "/BaiTapGioHangReact/index.html"
+  },
+  {
+    "revision": "45e0066a4eec66bdf70d",
+    "url": "/BaiTapGioHangReact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "85c4aea4f9730f32a0d0",
+    "url": "/BaiTapGioHangReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45e0066a4eec66bdf70d",
+    "url": "/BaiTapGioHangReact/static/js/2.e6bee8cb.chunk.js"
+  },
+  {
+    "revision": "7afc0a599953842b5fdf32e5f323b45d",
+    "url": "/BaiTapGioHangReact/static/js/2.e6bee8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c4aea4f9730f32a0d0",
+    "url": "/BaiTapGioHangReact/static/js/main.79955563.chunk.js"
+  },
+  {
+    "revision": "9b5dc895414b1e3e6796",
+    "url": "/BaiTapGioHangReact/static/js/runtime-main.60c43f68.js"
+  }
+]);
